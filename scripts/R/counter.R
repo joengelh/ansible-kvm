920ef02442a5d82664594c28b7aeb0b99badfd6a
joengelh <73387330+joengelh@users.noreply.github.com>
2020-11-28 23:28:14 +0100

    automated commit for update saving

--- scripts/R/counter.R
+++
@@ -1,5 +0,0 @@
-vmname <- read.delim(file = '/home/master/ansible-kvm/roles/clone-centos/vars/main.yml', header = FALSE, sep = "-")
-vmname$V3 <- vmname$V3 +1
-write.table(vmname, file = "/home/master/ansible-kvm/roles/clone-centos/vars/main.yml", sep = "-",
-            row.names = FALSE, col.names = FALSE, quote = FALSE)
-
