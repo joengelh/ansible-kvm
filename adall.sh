88df7da52057c2dddb215fb6fc47b0e43aa79cae
joengelh <73387330+joengelh@users.noreply.github.com>
2021-06-30 15:35:48 +0200

    automated commit for update saving

--- adall.sh
+++
@@ -1,6 +0,0 @@
-git config --global user.email "73387330+joengelh@users.noreply.github.com"
-git config --global user.name "joengelh"
-git pull
-git add --all
-git commit -m 'automated commit for update saving'
-git push
